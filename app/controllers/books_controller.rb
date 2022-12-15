50d85af27eeb09ca53eff83add7662af5d9cdf80
EC2 Default User <ec2-user@ip-172-31-5-65.ap-northeast-1.compute.internal>
2022-12-15 13:59:06 +0000

    kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,42 @@
 class BooksController < ApplicationController
-  def new
-  end
+
+
+
+  def create
+    book = Book.new(book_params)
+    book.save
+      flash[:notice] = "投稿が成功しました"
+    end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "投稿が成功しました"
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "10c1a3d8543340ef86098f86b8a8992b.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 end
