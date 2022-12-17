ae1746cd123dbf679bd3b7b041cef8d8d4217246
EC2 Default User <ec2-user@ip-172-31-5-65.ap-northeast-1.compute.internal>
2022-12-17 08:00:04 +0000

    kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,10 +3,15 @@ class BooksController < ApplicationController
 
 
   def create
-    book = Book.new(book_params)
-    book.save
-      flash[:notice] = "投稿が成功しました"
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
     end
+  end
 
   def index
     @book = Book.new
@@ -22,10 +27,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "投稿が成功しました"
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
